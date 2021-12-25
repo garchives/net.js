1bfb7a9c4dcb00b1c28d9fb7483cfb54b6de79e2
JinsYin <jinsyin@gmail.com>
2021-12-25 15:14:10 +0800

    [admin-react] 修改 Home 页面并删除 logo.svg

--- packages/admin-react/src/pages/home/Home.tsx
+++ packages/admin-react/src/pages/home/Home.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from '../../logo.svg';
+import { Link } from 'react-router-dom';
 import './Home.css';
 
 function Home() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
